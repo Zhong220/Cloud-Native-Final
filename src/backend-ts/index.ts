af176d51057b2399bae4fcde55125746e21e08cf
Y.C.Neo <elephantneo01@gmail.com>
2024-11-24 00:31:55 +0800

    fix(redis): redis, mysql exportfinish

--- src/backend-ts/index.ts
+++ src/backend-ts/index.ts
@@ -1,13 +1,11 @@
 import express, { Request, Response } from "express";
 import routes from "./routes/index.ts";
-import Redis from "ioredis";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
-const redis = new Redis(Number(process.env.REDIS_PORT) || 6380);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("CNS ts express backend is available.");
