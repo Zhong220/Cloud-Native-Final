99833aef4b63a7f04941d005a88ac12684723ff8
Y.C.Neo <elephantneo01@gmail.com>
2024-12-07 22:04:30 +0800

    fix(dependency)

--- src/backend-ts/routes/channel/repository.ts
+++
@@ -1,6 +0,0 @@
-import { DataModel, DtoModel} from "./model.ts";
-
-export default function loginRepository(model: DtoModel): DataModel {
-  const result: DataModel = { mail: "sdaf" };
-  return result;
-}
--- src/backend-ts/utils/redis.ts
+++
@@ -1,5 +0,0 @@
-import Redis from "ioredis";
-
-const redis = new Redis("redis");
-
-export default redis;
