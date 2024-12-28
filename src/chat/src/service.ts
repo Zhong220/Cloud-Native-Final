cc1f9405931e1f4c06fff9838ff187523e2fba73
EricLiu750501 <qazwsxedc123456789eric@gmail.com>
2024-12-28 21:49:23 +0800

    feat: chat/ creatAChatroom and generate ID

--- src/chat/src/service.ts
+++ src/chat/src/service.ts
@@ -113,3 +113,15 @@ async function storeMessageService(data: StoreMessageDto[]) {
     console.error(error);
   }
 }
+
+export function  generateChatroomID() {
+  const length = 6 
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters[randomIndex];
+  }
+  return result;
+
+}
