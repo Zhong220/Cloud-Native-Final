d9abb40d7eefb1bdbb8941787f1004c267362748
Y.C.Neo <elephantneo01@gmail.com>
2025-01-02 02:00:59 +0800

    build: deploy step 1 finish

--- src/chat/src/utils/redis.ts
+++ src/chat/src/utils/redis.ts
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 
-// const REDIS_URL = 6379; // for deploy
-const REDIS_URL = "redis"; // for local
+const REDIS_URL = 6379; // for deploy
+// const REDIS_URL = "redis"; // for local
 const redisClient = new Redis(REDIS_URL);
 
 export default redisClient;
